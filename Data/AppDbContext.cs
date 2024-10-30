81885534a9e5ecaa5f461f25e917832c0d21e13c
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2024-10-30 06:57:26 -0500

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using InventarioAVMR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioAVMR.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
