3f4b875f39ca62e1d5c14028faf2bd3cd78adaf3
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2024-11-05 11:28:01 -0500

    Se agregan controladores y vistas de bordado

---
+++ Controllers/BordadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioAVMR.Controllers
+{
+    public class BordadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
