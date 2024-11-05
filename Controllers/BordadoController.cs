0d93e04ec11e30b5d87a6517746eb439dde20ec7
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2024-11-05 12:40:08 -0500

    Vistas crear bordados y bordados realizados, imagenes y base de datos con ruta para la imagen

--- Controllers/BordadoController.cs
+++ Controllers/BordadoController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InventarioAVMR.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventarioAVMR.Controllers
 {
     public class BordadoController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public BordadoController (AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,6 +20,18 @@ namespace InventarioAVMR.Controllers
         public IActionResult BordadosRealizados()
         {
             return View();
+        }
+
+        public IActionResult CrearBordado()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BordadosRealizadosGet()
+        {
+            var bordados = await _context.Bordados.ToListAsync();
+            return View("BordadosRealizados", bordados);
         }
     }
 }
